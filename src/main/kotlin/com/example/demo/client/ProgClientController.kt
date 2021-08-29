942041aad84b0e39711d9b331ccfa507c8794728
sanimkhan <sanimkhan13@gmail.com>
2021-08-29 08:48:34 +0600

    clients, scheduler added

--- src/main/kotlin/com/example/demo/client/ProgClientController.kt
+++ src/main/kotlin/com/example/demo/client/ProgClientController.kt
@@ -1,7 +1,5 @@
 package com.example.demo.client
 
-import com.example.demo.service.SimpleService
-import com.example.demo.ResponseModel
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
---
+++ src/main/kotlin/com/example/demo/scheduler/TestJob.kt
@@ -0,0 +1,14 @@
+package com.example.demo.scheduler
+
+import io.micronaut.scheduling.annotation.Scheduled
+import jakarta.inject.Singleton
+
+@Singleton
+class TestJob {
+
+    @Scheduled(initialDelay = "1s", fixedDelay = "1m")
+    //@Scheduled(cron = "0 15 10 ? * MON")
+    internal fun oneSecAfterLastExecution() {
+        println("Executing oneSecAfterLastExecution()")
+    }
+}
